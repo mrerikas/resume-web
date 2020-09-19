6787b4eee668249a324789e80f9a1c121b793cd1
Erikas Misevicius <erikas.mise@gmail.com>
2020-09-19 13:10:13 +0300

    Form page finished again last

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -124,6 +124,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "erikutism@gmail.com"
-EMAIL_HOST_PASSWORD = "Karolina..2015"
+EMAIL_HOST_PASSWORD = os.environ.get("GMAIL_PASSWORD")
 EMAIL_PORT = "587"
 
